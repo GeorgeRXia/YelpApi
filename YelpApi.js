7168c9af7f1932f99e2068f24652efcecd34cbd6
tomatoandcheerio <Georxia@gmail.com>
2017-05-01 17:22:50 -0400

    intial commit

---
+++ YelpApi.js
@@ -0,0 +1,95 @@
+//Yelp API Assignment
+// Create a site with a search bar and a list of Philadelphia neighborhoods and/or towns and cities in the metro region. These can be listed in a dropdown menu or just as a collection of buttons/divs. There should also be a button or div that when clicked runs a function that: 
+// -  Searches the Yelp API for businesses in the selected neighborhood that match the search criteria from the search bar. 
+// - Lists the results on the page with some of the data associated with each result (name, address, image, etc.)
+// Additionally: 
+// - When a result is clicked, it opens the Yelp page for that business in a new window.
+// - When a user makes a new search, the old results go away and the new results  populate the page and are also clickable.
+// Use SASS and the forEach loop instead of the for loop.
+
+
+$(document).ready(function(){
+	var searchBar = document.getElementById("search-bar");
+	var submitButton = document.getElementById("submit-button");
+	var searchValue = "";
+
+	var results = document.getElementById("result");
+	var location = document.getElementsByClassName("locations");
+	var locationResult = "";
+var msg = "";
+
+var pictureArray = [];
+	for(var i = 0; i < location.length; i++){
+
+		location[i].addEventListener("click",function(){
+
+
+			locationResult = event.target.textContent;
+			console.log(locationResult);
+
+
+		})
+
+
+	}
+
+
+// div.style.backgroundImage="url('"+variable+"')"
+
+	submitButton.addEventListener("click", function(){
+		results.innerHTML = "";
+		pictureArray = [];
+		searchValue = searchBar.value;
+
+     $.ajax({
+         url: "https://yelp-search.herokuapp.com/search",
+         method: "GET",
+         data: {
+
+         	term: searchValue,
+         	location: locationResult
+        },
+         success: function(response){
+         	console.log(response);
+   
+
+         	for(let i = 0; i < 10; i++){
+         		
+         	 msg +=  "<div class= 'picture1'>" + "<a href=" + response.businesses[i].url + " target= '_blank' >" + response.businesses[i].name + "</br>" + " Address:" + response.businesses[i].location.address[0] +  " Philadelphia, Pa"+   + "</a>" + "</br> " + "</br>" + "</div>"; 
+
+         	 pictureArray.push(response.businesses[i].image_url);
+         	
+         	 
+
+         	 
+             }
+             
+             results.innerHTML = msg;
+             var picture1 = document.getelementsByClassName("picture1");
+
+             for(let i = 0; i < picture1.length; i++){
+
+
+             	picture1[i].addEventListener("mouseover", function(){
+
+
+
+
+             	})
+
+
+             }
+
+
+
+         }
+
+     })
+
+	})
+
+
+
+
+})
+
